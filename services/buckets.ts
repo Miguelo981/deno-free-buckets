6a407ac7e932fe06c6a0e38cccf331ddf9eb9782
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-10-18 15:37:23 +0200

    Change db url

--- services/buckets.ts
+++ services/buckets.ts
@@ -1,7 +1,7 @@
-const dbPath = './db';
+const dbUrl = Deno.env.get('ENV') === 'production' ? Deno.env.get('KV_URL') : './db';
 
 export async function findBucketFile(key: string): Promise<any> {
-  const kv = await Deno.openKv(dbPath);
+  const kv = await Deno.openKv(dbUrl);
   const res = await kv.get([key]);
 
   kv.close();
@@ -10,7 +10,7 @@ export async function findBucketFile(key: string): Promise<any> {
 }
 
 export async function setBucketFile(key: string, value: BufferSource) {
-  const kv = await Deno.openKv(dbPath);
+  const kv = await Deno.openKv(dbUrl);
   const res = await kv.set([key], value);
 
   kv.close();
